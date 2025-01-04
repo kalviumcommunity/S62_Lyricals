7c639c3fb479eed67037f7d3a6411ab673754492
claudiajerome07 <claudiajerome07@gmail.com>
2025-01-04 21:38:08 +0530

    connected mongo atlas

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
+if(process.env.NODE_ENV !=='PRODUCTION'){
+    require('dotenv').config()
+}
 const express=require('express')
+const connectDataBase=require('./DB/database.js')
 
 const app=express();
+const PORT=process.env.PORT || 3000
 
 app.get('/ping',(req,res)=>{
     return res.send('pong')
 })
 
-const PORT=3000
 app.listen(PORT,()=>{
+    connectDataBase()
     console.log(`server is running on http://localhost:${PORT}`)
 })
+
 
